95611f89e060458068c9697653877babd45220e6
Moldrok <alessio.aquilano@outlook.it>
2022-11-17 23:17:50 +0100

    v1.0.0

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author "Iakko(Malibù Tech Team)"
+description 'iak_wonkas_fudge_factory'
+
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+files {
+	"interiorproxies.meta"
+}
